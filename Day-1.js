05b99a33e145625aaac34b2c88d9be7abd062c45
unknown <williambenet@hotmail.com>
2023-02-22 01:29:59 -0300

    first

---
+++ Day-1.js
@@ -0,0 +1,51 @@
+// 1. Write a single line comment which says, comments can make code readable
+// Comments can make code readable
+
+// 2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+// 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/* Comments can make code readable, easy to reuse and informative */
+
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let string = 'string'
+let boolean = true
+let u = undefined
+let n = null
+
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof(string))
+console.log(typeof(boolean))
+console.log(typeof(u))
+console.log(typeof(n))
+
+// 6. Declare four variables without assigning values
+let a 
+let b 
+let c 
+let d
+
+// 7. Declare four variables with assigned values
+let e = 'letra E'
+let f = 'O proximo será um numero'
+let g = 1
+let h = true
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = 'William'
+let lastName = 'Benett'
+let maritalStatus = 'Single'
+let country = 'Brazil'
+let age = '22'
+console.log(`${firstName}\n${lastName}\n${maritalStatus}\n${country}\n${age}`)
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+console.log(firstName, lastName,  maritalStatus, country,  age )
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 22
+let yourAge = 30
+console.log(`I am ${myAge} years old \nYou are ${yourAge} years old`)
+
+
+
